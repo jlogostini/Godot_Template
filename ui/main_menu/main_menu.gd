d01a8cb0ab1da56fddae26102de8f706b1f0e5c4
JohnLogostini <johnlogostini16485@gmail.com>
2024-01-08 19:07:39 -0400

    Set the main menu to focus on the Play button on startup to fix gamepad input not working

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,4 +1,7 @@
 extends Control
+ 
+func _ready():
+	$margin_container/vertical_box_container/play_button.grab_focus()
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://worlds/template_world/template_world.tscn")
