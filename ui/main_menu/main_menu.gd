c5db0f4f85999b1ec4522e0001360a146d4aa8dc
JohnLogostini <johnlogostini16485@gmail.com>
2024-01-22 20:19:46 -0400

    Started integrating the WIP options menu into the main menu with gamepad support

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,15 +1,28 @@
 extends Control
- 
+
+@onready var main_menu = $main_menu 
+@onready var options_menu = $options_menu 
+var useingoptions = false
+
 func _ready():
-	$margin_container/vertical_box_container/play_button.grab_focus()
+	$main_menu/margin_container/vertical_box_container/play_button.grab_focus()
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://worlds/template_world/template_world.tscn")
-	
 
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://ui/options_menu/options_menu.tscn")
+	main_menu.visible = false
+	options_menu.visible = true
+	$options_menu/VBoxContainer/resolution_button.grab_focus()
+	useingoptions = true
 
+func _input(event):
+	if useingoptions:
+		if Input.is_action_just_pressed("ui_cancel"):
+			main_menu.visible = true
+			options_menu.visible = false
+			$main_menu/margin_container/vertical_box_container/options_button.grab_focus()
+			useingoptions = false
 
 func _on_quit_button_pressed():
 	get_tree().quit()
